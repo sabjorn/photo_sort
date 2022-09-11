42f5a1f31b0e2b56bf9e6a432aea811607d041b9
sabjorn <info@sabjorn.net>
2022-09-10 18:33:28 -0700

    fix date time and symlink relative issue

--- photo_sort.py
+++ photo_sort.py
@@ -3,6 +3,7 @@ import argparse
 import logging
 import pathlib
 import datetime
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input", help="input directory for photos")
@@ -19,39 +20,42 @@ if(args.verbose):
 
 logging.basicConfig(format="%(message)s", level=logging_level)
 
-
-valid_images = [".jpg", ".jpeg", ".mp4"]
+valid_images = [".jpg", ".jpeg", ".mp4", ".cr2"]
 for f in os.listdir(args.input):
     ext = os.path.splitext(f)[1]
 
     if ext.lower() not in valid_images:
+        logging.debug(f"no image found in {f}")
         continue
 
     logging.debug(f"image file: {f}")
 
     input_fullpath = os.path.join(args.input, f)
     logging.debug(input_fullpath)
+    
+    ti_m = os.path.getmtime(input_fullpath)
+    m_ti = time.ctime(ti_m)
+    t_obj = time.strptime(m_ti)
 
-    c_time = pathlib.Path(input_fullpath).stat().st_ctime
-    dt_c = datetime.datetime.fromtimestamp(c_time)
-    logging.debug(dt_c)
-
-    year = dt_c.strftime("%Y")
-    month = dt_c.strftime("%M")
-    day = dt_c.strftime("%d")
+    year = time.strftime("%Y", t_obj)
+    month = time.strftime("%m", t_obj)
+    day = time.strftime("%d", t_obj)
     output_directory = os.path.join("./", year, month, day)
     logging.debug(f"creating directory: {output_directory}")
     os.makedirs(output_directory, exist_ok=True)
  
-    hour = dt_c.strftime("%H")
-    minute = dt_c.strftime("%M")
-    second = dt_c.strftime("%S")
-    ms = dt_c.strftime("%f")
+    hour = time.strftime("%H", t_obj)
+    minute = time.strftime("%M", t_obj)
+    second = time.strftime("%S", t_obj)
+    ms = time.strftime("%f", t_obj)
     output_filename = f"{year}-{month}-{day}_{hour}.{minute}.{second}.{ms}{ext}"
 
     output_fullpath = os.path.join(output_directory, output_filename)
     logging.debug(f"writing symlink {f} to {output_fullpath}")
     try:
-        os.symlink(input_fullpath, output_fullpath)
+        os.symlink(
+                os.path.relpath(input_fullpath, output_directory),
+                output_fullpath
+                )
     except FileExistsError:
         logging.error(f"{f} already exists in {output_directory}, skipping")
